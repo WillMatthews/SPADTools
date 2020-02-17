4beaecb78bf5e94f1dddb39e9a9861875833a166
WillMatthews <will.a.matthews@me.com>
2020-02-17 14:58:03 +0000

    pre meeting

--- matlab_test/test2.m
+++ matlab_test/test2.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-L = logspace(-3,1,1000);
+L = logspace(-3,0.5,1000);
 h = 6.63e-34; %physical_constant("PlanchNumber");
 c = 3e8;
 lambda = 405e-9;
@@ -15,12 +15,12 @@ N = 14410;
 my_alpha   = A * pde / (N*Ep);
 
 my_count1   = N * my_alpha   * T * L ./ (1 + my_alpha   * deadtime * L);
-%deadtime = 1e-9;
+deadtime = 1e-9;
 %N = 14410;
 fprintf("d/N is %3.2e", deadtime/N);
 my_alpha   = A * pde / (N*Ep);
 my_count2   = N * my_alpha   * T * L ./ (1 + my_alpha   * deadtime * L);
-physical_limit = pde * L * A * T / Ep;
+physical_limit = L * A * T / Ep;
 
 
 figure();
@@ -28,7 +28,8 @@ semilogx(L,my_count1,'g--','linewidth',2);
 hold on;
 semilogx(L,my_count2,'r-', 'linewidth',2);
 semilogx(L,physical_limit,'b.','linewidth',2);
-legend("Real", "Modified", "Limit");
+semilogx(L,physical_limit*pde,'k.','linewidth',2);
+legend("Real", "Modified", "Limit-physical", "Limit-pde");
 grid on; grid minor;
 xlabel("Light Intensity at SiPM");
 ylabel("Count rate");
